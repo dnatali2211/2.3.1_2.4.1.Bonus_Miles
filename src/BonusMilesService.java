81b1721dd1236fcb89b1bdc4f6002bec0ae01a21
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-04-08 10:44:15 +0400

    new Java class

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int costMiles = 20;
+        int result = price / costMiles;
+        return result;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 17_357; // стоимость билета
-        int costMile = 20; // количество рублей за одну бонусную милю
-
-        int bonusMiles = (ticket / costMile);
-        System.out.println("Итоговый результат: " + bonusMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
